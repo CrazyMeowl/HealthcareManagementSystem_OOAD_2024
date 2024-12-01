21c7ac20cd3854a85e48b1d73ad6cf06d1d2daa7
Tran Cao Bao Phuc <62349601+CrazyMeowl@users.noreply.github.com>
2024-12-01 11:12:02 +0700

    Updated Register Screen
    Changed login method to phone number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, render_template
 import json
-import random
 import webbrowser
 app = Flask(__name__)
 
@@ -15,20 +14,28 @@ app_config = load_config()
 def main():
 	return render_template('index.html',name = app_config['name'])
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET','POST'])
 def login():
+	# POST
+	if request.method == 'POST':
+		phone_number = request.form['phone_number']
+		password = request.form['password']
+		return f"Phone Number: {phone_number}, Password: {password}"
+	# GET
 	return render_template('login.html',name = app_config['name'])
 
 
-
-@app.route('/authenticate', methods=['POST'])
-def authenticate():
-	ssn = request.form['ssn']
-	password = request.form['password']
-	return f"SSN: {ssn}, Password: {password}"
-
-@app.route('/register', methods=['GET'])
+@app.route('/register', methods=['GET','POST'])
 def register():
+	# POST
+	if request.method == 'POST':
+		full_name = request.form['full_name']
+		gender = request.form['gender']
+		date_of_birth = request.form['date_of_birth']
+		phone_number = request.form['phone_number']
+		password = request.form['password']
+		return f"Full Name: {full_name}, Phone Number: {phone_number}, Password: {password}, Gender: {gender}, Date Of Birth: {date_of_birth}"
+	# GET 
 	return render_template('register.html',name = app_config['name'])
 
 
